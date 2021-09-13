e6758798c212e9fd374bde6aaa650fa5e46583c9
Richard Guthrie <rguthrie@microsoft.com>
2021-09-13 10:35:36 -0700

    initialize repo

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright Istio Authors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+set -o errexit
+
+if [ "$#" -ne 2 ]; then
+    echo "Incorrect parameters"
+    echo "Usage: build.sh <version> <prefix>"
+    exit 1
+fi
+
+VERSION=$1
+PREFIX=$2
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+pushd "$SCRIPTDIR/productpage"
+  docker build --pull -t "${PREFIX}/bookinfo-productpage-v1:${VERSION}" -t "${PREFIX}/bookinfo-productpage-v1:latest" .
+popd
